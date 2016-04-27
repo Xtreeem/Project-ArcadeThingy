bcb9feb32bd76430cbafd2851a08266a1e34ef02
Hugo Tinglöf <hugo.tinglof@gmail.com>
2016-04-27 18:31:36 +0200

    Implemented the awesome new super flashy particlesystem

---
+++ Project ArcadeThingy/ContentManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_ArcadeThingy
+{
+    public static class ContentManager
+    {
+        public static List<Texture2D> Particles { get; private set; }
+        
+
+        public static void Load(Microsoft.Xna.Framework.Content.ContentManager _Content)
+        {
+            Particles = new List<Texture2D>();
+            Particles.Add(_Content.Load<Texture2D>("Particles\\Fire1"));
+            Particles.Add(_Content.Load<Texture2D>("Particles\\Fire2"));
+            Particles.Add(_Content.Load<Texture2D>("Particles\\Fire3"));
+        } 
+    }
+}
---
+++ Project ArcadeThingy/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_ArcadeThingy
+{
+    public static class Utilities
+    {
+        public static Random Random = new Random();
+        public static float NextFloat(float min, float max)
+        {
+            if (max < min)
+                throw new ArgumentException("max cannot be less than min");
+            float t = (float)Random.NextDouble() * (max - min) + min;
+            return t;
+        }
+    }
+}
